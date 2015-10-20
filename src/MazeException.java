bfd5f5978485529e1e171bc7b20fb3f0a851b40f
Emre Sonmez <ebs32@duke.edu>
2015-10-20 01:39:14 -0400

    debugging

---
+++ src/MazeException.java
@@ -0,0 +1,8 @@
+/**
+ * Created by emresonmez on 10/20/15.
+ */
+public class MazeException extends Exception {
+    public MazeException(String message){
+        super(message);
+    }
+}
--- src/QLearner.java
+++ src/QLearner.java
@@ -19,9 +19,21 @@ public class QLearner {
         this.reward = reward;
         this.step = step;
         this.maze = maze;
-        Q = new double[maze.length][maze.length][3];
+        Q = new double[maze.length][maze.length][4];
+        System.out.println(maze[5][6]);
         startX = 8;
         startY = 8;
+    }
+
+    public static void main(String[] args) {
+        SimpleMaze m = new SimpleMaze();
+        int[][] maze = m.generateMaze();
+        QLearner q = new QLearner(9, -1, maze);
+        try {
+            q.qLearning();
+        } catch (MazeException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -30,122 +42,178 @@ public class QLearner {
      * actions: proceed to any surrounding cell that is not 1
      * execute action, update Q table
      */
-    public void qLearning() {
+    public void qLearning() throws MazeException {
         // state: current cell
         // actions: proceed to any surrounding cell that is not 1
         // calculate q learning
+        Random r1 = new Random();
+        Random r2 = new Random();
+
         int steps = 0;
-        Cell current = new Cell(8,8,maze[startX][startY]);
-        while(current.getValue() != 9){
-            ArrayList<Cell> validCells = getValidCells(current);
-            int size = validCells.size();
+        int[] current = new int[2];
+        current[0] = 8;
+        current[1] = 8;
+        int x = current[0]; // goes down
+        int y = current[1]; // goes across
 
-            Random r1 = new Random();
-            int randomNum1 = r1.nextInt(100);
-            System.out.println("Random number: " + randomNum1);
-
-            Cell nextCell;
-            int r;
+        while (maze[x][y] != 9) {
+            int randomNum = randomInt(100, r1);
             int direction;
-            if(randomNum1 < epsilon){ // pick at random from valid cells
-                Random r2 = new Random();
-                int randomNum2 = r2.nextInt(size);
-                nextCell = validCells.get(randomNum2);
-                direction = getDirection(current,nextCell);
-            }else{ // pick max of Q[x][y]
-                direction = maxQdirection(current.getX(), current.getY());
-                int[] next  = getCoordinate(current,direction);
-                nextCell = getCell(next[0],next[1],validCells);
+            int[] next;
+            if (randomNum < epsilon) { // pick at random from valid cells
+                System.out.println("randomNum < epsilon (choosing randomly)");
+                ArrayList<int[]> validCells = getValidCells(x, y);
+                int size = validCells.size();
+                int randomNum2 = randomInt(size, r2);
+                next = validCells.get(randomNum2);
+                direction = getDirection(current, next);
+            } else { // pick max of Q[x][y]
+                System.out.println("randomNum > epsilon (choosing max)");
+                next = maxQCell(current);
+                direction = getDirection(current,next);
+
             }
-            r = nextCell.getValue();
-            updateQ(current.getX(),current.getY(),direction,r);
+            System.out.println("HELLOOO " + next[0] + " " + next[1]);
+            int reward = getReward(next[0],next[1]);
+            System.out.println("next x: " + next[0] + " next y: " + next[1] + " next value " + reward);
+            updateQ(x, y, direction, reward);
             steps++;
+
+            current = next;
+            x = current[0];
+            y = current[1];
         }
-        System.out.println("Goal acquired. Number of steps: " + steps);
     }
 
-    int getDirection(Cell from, Cell to){
-        int x1 = from.getX();
-        int x2 = to.getX();
-        int y1 = from.getY();
-        int y2 = to.getY();
-        // up (0)
-        if(y1 > y2){return 0;}
-        // right (1)
-        if(x2>x1){return 1;}
-        // down (2)
-        if(y2>y1){
-            return 2;
-        }
-        // left (3)
-        if(x2<x1){
-            return 3;
-        }
-        return -1;
+    int randomInt(int limit, Random r){
+        return r.nextInt(limit);
     }
 
-    Cell getCell(int x, int y, ArrayList<Cell> cells){
-        for(Cell c:cells){
-            if((c.getX() == x) && (c.getY() == y)){
-                return c;
+    ArrayList<int[]> getValidCells(int x, int y) {
+        ArrayList<int[]> cells = new ArrayList<>();
+        System.out.println(maze.length);
+        if(x+1 < maze.length){
+            int[] ret = new int[2];
+            ret[0] = x+1;
+            ret[1] = y;
+            if(maze[ret[0]][ret[1]] != 1){
+                cells.add(ret);
             }
         }
-        return null;
-    }
 
-    void updateQ(int x, int y, int direction, int r){
-        Q[x][y][direction] = alpha*(Q[x][y][direction] - (r+gamma*maxQval(x,y)));
-    }
+        if(x-1 >= 0){
+            int[] ret = new int[2];
+            ret[0] = x-1;
+            ret[1] = y;
+            if(maze[ret[0]][ret[1]] != 1){
+                cells.add(ret);
+            }
+        }
 
-    int[] getCoordinate(Cell cell,int direction){
-        int x = 0;
-        int y = 0;
-        int[] result = new int[2];
-        if(direction == 0){
-            x = cell.getX()-1;
-            y = cell.getY();
+        if(y+1 < maze.length){
+            int[] ret = new int[2];
+            ret[0] = x;
+            ret[1] = y+1;
+            if(maze[ret[0]][ret[1]] != 1){
+                cells.add(ret);
+            }
         }
-        if(direction == 1){
-            x = cell.getX();
-            y = cell.getY()+1;
+
+        if(y-1 >= 0){
+            int[] ret = new int[2];
+            ret[0] = x;
+            ret[1] = y-1;
+            if(maze[ret[0]][ret[1]] != 1){
+                cells.add(ret);
+            }
         }
-        if(direction == 2){
-            x = cell.getX()+1;
-            y = cell.getY();
+        for(int[] k:cells){
+            System.out.println(k[0] + " " + k[1]);
         }
-        if(direction == 3){
-            x = cell.getX();
-            y = cell.getY()+1;
-        }
-        result[0] = x;
-        result[1] = y;
-        return result;
+        return cells;
     }
 
     /**
-     * returns direction of greatest Q value
-     * @param x
-     * @param y
+     * returns direction
+     * @param from
+     * @param to
      * @return
      */
-    private int maxQdirection(int x, int y){
-        double max = 0;
-        int direction = 0;
-        for(int i = 0; i < 3; i++){
-            double val = Q[x][y][i];
-            if(val < max){
-                max = val;
-                direction = i;
-            }
+    int getDirection(int[] from, int[] to) throws MazeException {
+        if (from[0] > to[0]) { // up
+            return 0;
         }
-        return direction;
+        if (from[1] < to[1]) { // right
+            return 1;
+        }
+        if (from[0] < to[0]) { // down
+            return 2;
+        }
+        if (from[1] > to[1]) { // left
+            return 3;
+        }
+        throw new MazeException("Invalid direction.");
     }
 
-    private double maxQval(int x, int y){
+    /**
+     * gets next cell from direction
+     * @param x
+     * @param y
+     * @param direction
+     * @return
+     */
+    int[] nextCell(int x, int y, int direction) throws MazeException {
+        int[] ret = new int[2];
+        switch (direction) {
+            case 0:
+                ret[0] = x-1;
+                ret[1] = y;
+            case 1:
+                ret[0] = x;
+                ret[1] = y+1;
+            case 2:
+                ret[0] = x+1;
+                ret[1] = y;
+            case 3:
+                ret[0] = x;
+                ret[1] = y-1;
+        }
+        if(maze[ret[0]][ret[1]] == 1){
+            throw new MazeException("next cell error for (" + x + ", " + y + ", " + direction + ")");
+        }
+        return ret;
+    }
+
+    /**
+     * updates Q matrix
+     * @param x
+     * @param y
+     * @param direction
+     * @param r
+     */
+    void updateQ(int x, int y, int direction, int r) {
+        Q[x][y][direction] += alpha * (r + gamma * maxQVal(x, y) - Q[x][y][direction]);
+    }
+
+    private int[] maxQCell(int[] from) throws MazeException {
+        int[] ret = new int[2];
+        double max = -1;
+        ArrayList<int[]> valid = getValidCells(from[0],from[1]);
+        for(int[] k:valid){
+            System.out.println(from[0] + " " + from[1] + " " + getDirection(from,k));
+            if(Q[from[0]][from[1]][getDirection(from,k)] >= max){
+                max = Q[from[0]][from[1]][getDirection(from,k)];
+                ret = k;
+            }
+        }
+        return ret;
+    }
+
+    private double maxQVal(int x, int y) {
         double max = 0;
-        for(int i = 0; i < 3; i++){
+        for (int i = 0; i < 3; i++) {
             double val = Q[x][y][i];
-            if(val < max){
+            if ((val > max) && val !=0){
                 max = val;
             }
         }
@@ -155,56 +223,19 @@ public class QLearner {
 
     /**
      * reward for going to a certain cell
-     * @param cell
+     * @param x
+     * @param y
      * @return
      */
-    private int getReward(Cell cell){
-        if(cell.getValue() == 0){
+    private int getReward(int x, int y) throws MazeException {
+        if(maze[x][y] == 0){
             return step;
         }
-        if(cell.getValue() == 9){
+        if(maze[x][y] == 9){
+            System.out.println("x: " + x + "y: " + y);
             return reward;
         }
-        return 0;
+        throw new MazeException("Invalid cell to obtain reward from (" + x + ", " + y + ")");
     }
 
-    /**
-     * returns valid cells as arraylist for any location on grid
-     * @param currentCell
-     * @return
-     */
-    public ArrayList<Cell> getValidCells(Cell currentCell){
-        ArrayList<Cell> result = new ArrayList<>();
-        int x = currentCell.getX();
-        int y = currentCell.getY();
-        // check up
-        if(y+1 < maze.length){
-            if(maze[x][y+1] != 1){
-                Cell cUp = new Cell(x,y+1,maze[x][y+1]);
-                result.add(cUp);
-            }
-        }
-        // check right
-        if(x+1 < maze.length) {
-            if (maze[x + 1][y] != 1) {
-                Cell cRight = new Cell(x + 1, y, maze[x + 1][y]);
-                result.add(cRight);
-            }
-        }
-        // check down
-        if(y-1 >= 0){
-            if(maze[x][y-1] != 1){
-                Cell cDown = new Cell(x,y-1,maze[x][y+1]);
-                result.add(cDown);
-            }
-        }
-        // check left
-        if(x-1 >= 0){
-            if(maze[x-1][y] != 1){
-                Cell cLeft = new Cell(x-1,y,maze[x-1][y]);
-                result.add(cLeft);
-            }
-        }
-        return result;
-    }
 }
